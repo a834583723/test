649056950cf3113eaf247e4c9d47a6bd49786797
wang miao <wangmiaopeng@qq.com>
2019-01-25 18:04:46 +0800

    'xiangmu12'

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,8 +1,12 @@
+import allure
+
 class Test_001:
+    @allure.step(title="我是测试步骤1")
     def test_001(self):
         print('0001')
         assert True
 
+    @allure.step(title="我是测试步骤22222")
     def test_002(self):
         print('002')
         assert False
